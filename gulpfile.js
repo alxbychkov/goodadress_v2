1dc2e9489209c1d9c735d8e2a0a833d2e9185482
Alexander Bychkov <alx.bychkov@me.com>
2019-08-24 13:40:07 +0300

    Add files to repository

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    concat = require('gulp-concat'),
+    cleanCSS = require('gulp-clean-css'),
+    cssnano = require('gulp-cssnano'), // Подключаем пакет для минификации CSS
+    rename = require('gulp-rename');
+
+gulp.task('sass', function () { // Создаем таск Sass
+    return gulp.src('app/scss/**/*.scss') // Берем источник
+        .pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('app/css'))
+        .pipe(cleanCSS())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
+        .pipe(browserSync.reload({ stream: true })) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
+    browserSync({ // Выполняем browserSync
+        server: { // Определяем параметры сервера
+            baseDir: 'app' // Директория для сервера - app
+        },
+        notify: false // Отключаем уведомления
+    });
+});
+
+gulp.task('watch', function () {
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass')); // Наблюдение за sass файлами
+});
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+
+
