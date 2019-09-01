8870252f3655ff52dd911fe31ece36540bc68b47
Alexander Bychkov <alx.bychkov@me.com>
2019-09-01 13:10:45 +0300

    add adreses.html and style

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,18 @@ $(document).ready(function () {
     $('.burger').click(function () {
         $('.navigation-min').slideToggle("slow");
     });
+    $(window).resize(function () {
+        if ($('.navigation-min').css('display') != 'none') {
+            $('.navigation-min').hide();
+        }
+    });
+    $('.nav-item').click(function () {
+        let idName = $(this).attr('data');
+        if (idName) {
+            let offset = $(idName).offset();
+            $('html, body').animate({
+                'scrollTop': offset.top - 32
+            }, 1000);
+        }
+    });
 });
