8a2a8a45fe79c8a19047587cd9f57f434ab13c62
Alexander Bychkov <alx.bychkov@me.com>
2019-09-05 21:07:06 +0300

    slider with php and some hover effects

--- app/modules/slider_section.php
+++ app/modules/slider_section.php
@@ -1,51 +1,36 @@
+<?php
+include('php/db_connect.php');
+include('php/functions.php');
+
+$slide_item = [];
+$error = '';
+$query = 'SELECT `picture_name`, `address`, `price` FROM `addresses`';
+$result = mysqli_query($link, $query);
+if ($result) {
+    while ($item = mysqli_fetch_assoc($result)) {
+        $slide_item[] = $item;
+    }
+} else {
+    $error .= 'Неверный запрос';
+}
+
+
+?>
 <section class="section adreses-section" id="adreses">
     <div class="title">
         <h2><?= $title; ?></h2>
     </div>
     <div class="content">
         <div class="owl-carousel slider">
+            <?php foreach ($slide_item as $value) : ?>
             <div class="slider-item">
                 <a href=""
-                    style="background: url(adres_img/1.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">18 000</button>
+                    style="background: url(adres_img/<?= $value['picture_name'] ?>); background-size: cover; background-repeat: no-repeat">
+                    <p><?= $value['address']; ?></p>
+                    <button class="button"><?= $value['price']; ?></button>
                 </a>
             </div>
-            <div class="slider-item">
-                <a href=""
-                    style="background: url(adres_img/2.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">22 000</button>
-                </a>
-            </div>
-            <div class="slider-item">
-                <a href=""
-                    style="background: url(adres_img/3.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">11 000</button>
-                </a>
-            </div>
-            <div class="slider-item">
-                <a href=""
-                    style="background: url(adres_img/6.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">16 000</button>
-                </a>
-            </div>
-            <div class="slider-item">
-                <a href=""
-                    style="background: url(adres_img/5.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">20 000</button>
-                </a>
-            </div>
-            <div class="slider-item">
-                <a href=""
-                    style="background: url(adres_img/4.jpg); background-size: cover; background-repeat: no-repeat">
-                    <p>Ул. Пятницкая д. 38</p>
-                    <button class="button">25 000</button>
-                </a>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </section>
